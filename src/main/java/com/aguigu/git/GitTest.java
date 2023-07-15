30fc8cdaaee7980535d9001a00defe6539aed9a7
wdtiantian <wd666@163.com>
2023-07-15 17:58:47 +0800

    码云提交

--- src/main/java/com/aguigu/git/GitTest.java
+++ src/main/java/com/aguigu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.aguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("****************");
     }
 }
